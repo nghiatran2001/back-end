f63f3844f580e718c0b327b4bbb0fc24b1e0f004
nghiatran2001 <trannguyenhuunghia6@gmail.com>
2023-08-15 19:14:32 +0700

    update

--- models/categories.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-const Category = mongoose.Schema(
-  {
-    nameCategory: String,
-    description: String,
-  },
-  { timestamps: true }
-);
-export const CategoryModel = mongoose.model("category", Category);
