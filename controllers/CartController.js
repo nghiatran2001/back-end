f4643731beb6e59aaa3dd97d56f83e53e43fb10c
Nghia Tran <trannguyenhuunghia6@gmail.com>
2023-12-18 17:22:51 +0700

    edit

--- controllers/CartController.js
+++ controllers/CartController.js
@@ -28,4 +28,34 @@ const getAllCart = async (req, res) => {
   }
 };
 
-module.exports = { addCart, getAllCart };
+const getEmail = async (req, res) => {
+  try {
+    const { email } = req.params;
+    const carts = await Cart.find(
+      { email: email },
+      { idProduct: 1, email: 1, quantity: 1 }
+    );
+    const idProduct = carts.map((e) => e.idProduct);
+    const prouducts = await Product.find(
+      { _id: idProduct },
+      { nameProduct: 1, image: 1, sellPrice: 1 }
+    );
+    const finalData = carts.map((cart) => {
+      prouducts.some((product) => {
+        if (product._id.toString() === cart.idProduct) {
+          cart._doc.nameProduct = product.nameProduct;
+          cart._doc.image = product.image;
+          cart._doc.sellPrice = product.sellPrice;
+          return true;
+        }
+        return false;
+      });
+      return cart;
+    });
+    res.send(finalData);
+  } catch (error) {
+    res.status(500).send("Internal server error");
+  }
+};
+
+module.exports = { addCart, getAllCart, getEmail };
--- routes/cart.js
+++ routes/cart.js
@@ -1,10 +1,15 @@
 const express = require("express");
 
-const { addCart, getAllCart } = require("../controllers/CartController");
+const {
+  addCart,
+  getAllCart,
+  getEmail,
+} = require("../controllers/CartController");
 
 const router = express.Router();
 
 router.post("/add", addCart);
 router.get("/getall", getAllCart);
+router.get("/:email", getEmail);
 
 module.exports = router;
